7133a551d64f162476c6872e7c28db2e35698008
Orbb2010 <orbb2010@inbox.ru>
2024-01-24 18:43:55 +0500

    js-features

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,8 +2,7 @@ const cookie = document.getElementById('cookie');
 const clickerCounter = document.getElementById('clicker__counter');
 let clicker__counter = 0;
 cookie.onclick = function () {
-  cookie.width = 256;
-  cookie.height = 194;
   clicker__counter++;
-  clickerCounter.textContent(clicker__counter);
+  clickerCounter.textContent = clicker__counter;
+  cookie.classList.toggle('clicker__cookie');
 }
---
+++ js-features/mole-game/task.js
@@ -0,0 +1,23 @@
+const dead = document.getElementById('dead');
+const lost = document.getElementById('lost');
+let counterDead = 0;
+let counterLost = 0;
+getHole = index => document.getElementById(`hole${index}`);
+for (let holeIndex = 1; holeIndex < 10; holeIndex++) {
+  let hole = getHole(holeIndex);
+  hole.addEventListener("click", function() {
+    if (hole.classList.contains('hole_has-mole')) {
+    counterDead++;
+    dead.textContent = counterDead;
+    } else {
+    counterLost++;
+    lost.textContent = counterLost;
+    }
+    });
+}
+if (counterDead == 3) {
+  alert("win");
+  counterDead = 0;
+  counterLost = 0;
+  dead.textContent = counterDead;
+}
